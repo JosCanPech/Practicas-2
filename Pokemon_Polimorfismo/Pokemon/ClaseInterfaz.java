bf52d92da1b0ffd5c826931d74b4f4996fd19061
JosCanPech <132014654+JosCanPech@users.noreply.github.com>
2023-10-27 22:48:48 -0600

    Create ClaseInterfaz.java

---
+++ Pokemon_Polimorfismo/Pokemon/ClaseInterfaz.java
@@ -0,0 +1,14 @@
+package Pokemon;
+
+/**
+ *
+ * @author josue
+ */
+public interface ClaseInterfaz {
+    
+    public void CalcularDanio(int danio);
+    public boolean ConcretarAtaque(Movimiento movimiento, Pokemon pokemon, Tipo Tipo, TipoMovimiento TipoMovimiento);
+    public void Ataque(int m, Pokemon pokemon, Tipo Tipo, TipoMovimiento TipoMovmiento );
+    public void RecibirAtaque(Movimiento movimiento, double efectividad); 
+    
+}
