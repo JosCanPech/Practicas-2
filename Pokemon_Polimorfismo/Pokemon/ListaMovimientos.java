2e157286750f3d8a931c818f1f1385ec43225506
JosCanPech <132014654+JosCanPech@users.noreply.github.com>
2023-10-27 22:18:03 -0600

    Update ListaMovimientos.java

--- Pokemon_Polimorfismo/Pokemon/ListaMovimientos.java
+++ Pokemon_Polimorfismo/Pokemon/ListaMovimientos.java
@@ -1,43 +1,38 @@
 package Pokemon;
 
-//import static Pokemon.Tipo.AGUA;
-//import static Pokemon.Tipo.HIELO;
-
 import java.util.ArrayList;
 
 public class ListaMovimientos {
     
-    /*private String nombre;
-    private int puntosDeAtaque;
-    private Tipo Tipo;
-    private int pp;*/
-    
-    /*public ListaMovimientos(String nombre, int puntosDeAtaque, Tipo tipo, int pp){
-        this.nombre = nombre;
-        this.puntosDeAtaque = puntosDeAtaque;
-        this.Tipo = Tipo;
-        this.pp = pp;
-    }*/
-    
-    
-    private ArrayList<Movimiento> movimientos = new ArrayList<>();
+     ArrayList<Movimiento> movimientos = new ArrayList<>();
     
    
     
     public ListaMovimientos(){
+       
         // Movimientos de tipo agua
-        movimientos.add(new Movimiento("Pistola Agua Water Gun",40,Tipo.AGUA,25 ));
-        movimientos.add(new Movimiento("Golpe de cabeza", 70,Tipo.AGUA,24 ));
-        movimientos.add(new Movimiento("Placaje", 40,Tipo.AGUA,56 ));
-        movimientos.add(new Movimiento("Cabezazo", 130,Tipo.AGUA,16 ));        
+        movimientos.add(new Movimiento("Pistola Agua Water Gun",40,TipoMovimiento.FISICO,25 ));
+        movimientos.add(new Movimiento("Golpe de cabeza", 70,TipoMovimiento.FISICO,24 ));
+        movimientos.add(new Movimiento("Placaje", 40,TipoMovimiento.FISICO,56 ));
+        movimientos.add(new Movimiento("Cabezazo", 130,TipoMovimiento.FISICO,16 ));        
         
         
         //Movimientos de tipo Ice
-        movimientos.add(new Movimiento("Tumba rocas", 15,Tipo.HIELO,60 ));
-        movimientos.add(new Movimiento("Terremoto", 10,Tipo.HIELO,100 ));
-        movimientos.add(new Movimiento("Bola hielo", 30,Tipo.HIELO,30 ));
-        movimientos.add(new Movimiento("Nieve Polvo", 40,Tipo.HIELO,40 ));
+        movimientos.add(new Movimiento("Tumba rocas", 15,TipoMovimiento.FISICO,60 ));
+        movimientos.add(new Movimiento("Terremoto",10,TipoMovimiento.FISICO,100 ));
+        movimientos.add(new Movimiento("Bola hielo", 30,TipoMovimiento.FISICO,30 ));
+        movimientos.add(new Movimiento("Nieve Polvo", 40,TipoMovimiento.ESPECIAL,40 ));
+        
+        
+        //movimientos de pidgey
+        movimientos.add(new Movimiento("Ataque rapido",40,TipoMovimiento.FISICO,30));
+        movimientos.add(new Movimiento("Ataque ala",60,TipoMovimiento.ESPECIAL,35));
+        //Placaje reutilzado para pidgey
+        movimientos.add(new Movimiento("Golpe aereo",60,TipoMovimiento.FISICO,20));
     }   
+    
+    
+    
     
     public Movimiento buscarMovimientoPorNombre(String nombre){
         for (Movimiento movimiento : movimientos){
